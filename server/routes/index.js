62b0c78958c30f4e8dcd0a20b8093ba4ed83849d
jy.jeong <jy.jeong@e-ppn.com>
2019-02-26 16:08:58 +0900

    create server and client directories

---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  fs.readFile("./server/data/example.txt", 'utf8', (err, data) => {
+    res.render('index', { title: 'Wordy', contents: data });
+  })
+});
+
+module.exports = router;
